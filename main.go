00a4311e1818533127e385788fd84dc84d1d37db
Hai Yue <hai.yue@ingka.com>
2023-10-15 17:53:36 +0800

    solve missing log issue

--- main.go
+++ main.go
@@ -10,19 +10,22 @@ import (
 func main() {
 	// multiple threads stress test
 	for i := 0; i < 2; i++ {
-		otellog.Infof("lablador log record: %s", strconv.Itoa(i)+"thInfof image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otellog.Tracef("lablador log record: %s", strconv.Itoa(i)+"thTracef image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otellog.Debugf("lablador log record: %s", strconv.Itoa(i)+"thDebugf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otellog.Warnf("lablador log record: %s", strconv.Itoa(i)+"thWarnf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otellog.Errorf("lablador log record: %s", strconv.Itoa(i)+"thErrorf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
 		//go otellog.Fatalf("lablador log record: %s", strconv.Itoa(i)+"th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
 		//go otellog.Panicf("lablador log record: %s", strconv.Itoa(i)+"th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otellog.Printf("lablador log record: %s", strconv.Itoa(i)+"th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
 
-		otelfmt.Sprintf("lablador log record: %s", strconv.Itoa(i)+"th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otelfmt.Errorf("lablador log record: %s", strconv.Itoa(i)+"th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
-		go otelfmt.Println(strconv.Itoa(i) + "th image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		otellog.Infof("lablador log record: %s", strconv.Itoa(i)+"Infof image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otellog.Tracef("lablador log record: %s", strconv.Itoa(i)+"Tracef image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otellog.Debugf("lablador log record: %s", strconv.Itoa(i)+"Debugf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otellog.Warnf("lablador log record: %s", strconv.Itoa(i)+"Warnf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otellog.Errorf("lablador log record: %s", strconv.Itoa(i)+"Errorf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otellog.Printf("lablador log record: %s", strconv.Itoa(i)+"Printf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
 
-		//time.Sleep(1000)
+		otelfmt.Sprintf("lablador log record: %s", strconv.Itoa(i)+"Sprintf image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+
+		go otelfmt.Errorf("lablador log record: %s", strconv.Itoa(i)+"Errorf2 image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+		go otelfmt.Println(strconv.Itoa(i) + "Println image - after last shop front series I imagined IKEA shop front in beautiful lane of Italy or Spain :) it will be one of a kind #unique #bedifferent")
+
 	}
+	channel := make(chan int)
+	channel <- 1
 }
